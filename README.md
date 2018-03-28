1a26251be4993d3c6cbd0906153e27ca44e5ee2d
Victor Balan <victor.balan@fusionops.com>
2018-03-28 12:23:48 +0300

    Add lebab

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ usage:
 node index.js <file_name>
 opts:
     -w: write back to file
+    -l: use lebab with safe operations
+    -lu: use lebab with safe operations **AND UNSAFE OPERATIONS**
+
+lebab: https://github.com/lebab/lebab
 
 The script will replace this.$$('elem') with this.qs('elem') and this.$.elem with this.qs('#elem') and add a mixin to a BaseElementBehavior
 which should contain the qs(querySelector) method (eg. this.shadowRoot.querySelector(arg)).
+
 The base element behavior will be also added if fire event('fire') is used.
 
 The script is a bit hardcoded so it will work only if the element has defined the properties before any methods.
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let configExtractor = require('./configExtractor');
 let configWriter = require('./configWriter');
 let es6Converter = require('./es6Converter');
+let lebab = require('lebab');
 
 if (process.argv.length < 3) {
     console.log('usage: node index.js <file_name> flags')
@@ -8,20 +9,35 @@ if (process.argv.length < 3) {
 }
 let fileName = '';
 let writeFile = false;
+let useLebab = false;
+let useLebabUnsafe = false;
 for (let i = 1; i < process.argv.length; i++) {
     let arg = process.argv[i];
     switch (arg) {
         case '-w':
             writeFile = true;
             break;
+        case '-l':
+            useLebab = true;
+            break;
+        case '-lu':
+            useLebabUnsafe = true;
+            break;
         default:
             fileName = arg;
     }
 }
-console.log('running convertor for ' + fileName + ' with write file:' + writeFile);
+console.log('running convertor for ', fileName, ' with write file:', writeFile, 'use lebab: ', useLebab, 'use lebab unsafe: ', useLebabUnsafe);
 
 let polymerConfig = configExtractor.extractJsonConfigFromFile(fileName);
 let es6class = es6Converter.convertToES6(polymerConfig);
+let safeLebab = ['arrow', 'for-of', 'for-each', 'arg-rest', 'arg-spread', 'obj-method', 'obj-shorthand', 'exponent', 'multi-var'];
+if(useLebab) {
+    es6class = lebab.transform(es6class, safeLebab).code;
+}
+if(useLebabUnsafe) {
+    es6class = lebab.transform(es6class, safeLebab.concat(['let'])).code;
+}
 if (writeFile) {
     configWriter.writeES6Class(fileName, polymerConfig.start, polymerConfig.end, es6class);
 } else {
